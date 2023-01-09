d9e757b64d69faa22e5d55c042f77f21136d9f4e
Thalles Alves <104025889+Th3AI99la@users.noreply.github.com>
2023-01-09 13:54:56 -0300

    Add files via upload

---
+++ js/code.js
@@ -0,0 +1,24 @@
+const input = document.querySelector("input");
+const qrcode = document.querySelector("#qrcode");
+
+
+document.addEventListener("keypress", (e) => {
+
+    if (e.key === "Enter") {
+        genQRCode ();
+    }
+
+
+
+});
+
+
+function genQRCode () {
+
+    if (!input.value) return;
+
+    qrcode.src = `https://api.qrserver.com/v1/create-qr-code/?size=350x150&data=${input.value}`;
+
+
+}
+
